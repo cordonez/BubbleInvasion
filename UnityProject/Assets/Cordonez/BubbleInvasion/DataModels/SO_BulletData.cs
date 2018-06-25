9203c31208c37d1e1c2e242ff175092c8e2b289b
Cayetano Ordóñez Badillo <cordonez.main@gmail.com>
2018-06-25 22:39:54 +0200

    Updated ref types for CSO

--- UnityProject/Assets/Cordonez/BubbleInvasion/DataModels/SO_BulletData.cs
+++ UnityProject/Assets/Cordonez/BubbleInvasion/DataModels/SO_BulletData.cs
@@ -4,5 +4,12 @@
 	using UnityEngine;
 
 	[CreateAssetMenu(menuName = MenuPath.CUSTOM_VARIABLES + "SO_BulletData")]
-	public class SO_BulletData : CustomScriptableObject<BulletData> { }
+	public class SO_BulletData : CustomScriptableObject<BulletData>
+	{
+		public override void ResetToDefault()
+		{
+			m_runtimeValue.BulletPrefab = m_value.BulletPrefab;
+			m_runtimeValue.Speed = m_value.Speed;
+		}
+	}
 }
--- UnityProject/Assets/Cordonez/BubbleInvasion/DataModels/SO_WeaponData.cs
+++ UnityProject/Assets/Cordonez/BubbleInvasion/DataModels/SO_WeaponData.cs
@@ -4,5 +4,12 @@
 	using UnityEngine;
 
 	[CreateAssetMenu(menuName = MenuPath.CUSTOM_VARIABLES + "SO_WeaponData")]
-	public class SO_WeaponData : CustomScriptableObject<WeaponData> { }
+	public class SO_WeaponData : CustomScriptableObject<WeaponData>
+	{
+		public override void ResetToDefault()
+		{
+			m_runtimeValue.WeaponParams = m_value.WeaponParams;
+			m_runtimeValue.BulletData = m_value.BulletData;
+		}
+	}
 }
